1a50e3231b1d9635c281eddcede4b1e0d224fde5
Bryan J <bryandavila520@gmail.com>
2023-10-04 12:52:48 +0200

    04/10/2023 part2

--- LoginAndroid_29_9_2023/app/src/main/java/com/example/loginandroid_29_9_2023/MainActivity.java
+++ LoginAndroid_29_9_2023/app/src/main/java/com/example/loginandroid_29_9_2023/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.loginandroid_29_9_2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtEmail;
+
+    private EditText edtPassword;
+    private Button brnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+
     }
+    private void initComponents(){
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        brnLogin = findViewById(R.id.btnLogin);
+        brnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+    }
+
+
 }
